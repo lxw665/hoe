c9f143e7c908374f0e471d1ffed0d41b8f778152
lxw665 <1051823195@qq.com>
2022-12-16 23:43:56 +0800

    first commit

---
+++ src/main/java/com/lxw/hoe/HoeApplication.java
@@ -0,0 +1,13 @@
+package com.lxw.hoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxw/hoe/HoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxw.hoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
